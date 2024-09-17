2a757dde0f87d56272586ae593fba97c517819b2
endermendoza24 <enderalonso24@gmail.com>
2024-09-16 19:00:53 -0600

    Agregada una función que en tiempo real va verificando que se esté introduciendo correctamente el formato email

--- SistemaBiblioteca/Client/Utilidad/UsuarioLogin.cs
+++ SistemaBiblioteca/Client/Utilidad/UsuarioLogin.cs
@@ -4,7 +4,8 @@ namespace SistemaBiblioteca.Client.Utilidad
 {
     public class UsuarioLogin
     {
-        [Required(ErrorMessage = "El correo es requerida.")]
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo debe tener un formato válido, como usuario@dominio.com.")]
         public string correo { get; set; }
         [Required(ErrorMessage = "La contraseña es requerida.")]
         public string clave { get; set; }
